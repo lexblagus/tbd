c50c2948589520d384fad5b3aceb2ea67bb949cb
Lex Blagus <lex@x128.com.br>
2015-12-16 18:49:56 -0200

    JSON sample

--- script/conf.js
+++ script/conf.js
@@ -1,3 +1,89 @@
+var config = {
+	// Fabrics and colors configuration
+	"fabrics" : {
+		"my-color-1" : {
+			"rgb" : "#FF00CC",
+			"pantone" : "Gold yellow",
+			"tissue" : "Nylon"
+		},
+		"my-color-2" : {
+			"rgb" : "#00CCFF",
+			"pantone" : "Petroleum blue",
+			"tissue" : "Dyneema"
+		},
+		"my-color-3" : {
+			"rgb" : "#0CF1F6",
+			"pantone" : "Black fish",
+			"tissue" : "Kevlar"
+		},
+		"my-color-4" : {
+			"rgb" : "#F1CCFF",
+			"pantone" : "Forest green",
+			"tissue" : "Nylon"
+		},
+		"my-color-5" : {
+			"rgb" : "#CCCCFF",
+			"pantone" : "White cocaine",
+			"tissue" : "silk"
+		},
+	},
+
+	// Parametros de montagem dos gliders
+	"rules" : {
+		"lotus_petroleo" : {
+			"config" : {
+				"upper-front" : {
+					"available-colors" : ["my-color-1", "my-color-3" , "my-color-5"]
+				},
+				"upper-middle" : {
+					"available-colors" : ["my-color-2", "my-color-3" , "my-color-4"]
+				},
+				"upper-back" : {
+					"available-colors" : ["my-color-1", "my-color-2" , "my-color-3"]
+				},
+			}
+		},
+		"lotus_xpto" : {
+			"config" : {
+				"upper-front" : {
+					"available-colors" : ["my-color-1", "my-color-3" , "my-color-5"]
+				},
+				"upper-middle" : {
+					"available-colors" : ["my-color-2", "my-color-3" , "my-color-4"]
+				},
+				"upper-back" : {
+					"available-colors" : ["my-color-1", "my-color-2" , "my-color-3"]
+				},
+			}
+		},
+		"lotus_xpto2" : {
+			"config" : {
+				"upper-front" : {
+					"available-colors" : ["my-color-1", "my-color-3" , "my-color-5"]
+				},
+				"upper-middle" : {
+					"available-colors" : ["my-color-2", "my-color-3" , "my-color-4"]
+				},
+				"upper-back" : {
+					"available-colors" : ["my-color-1", "my-color-2" , "my-color-3"]
+				},
+			}
+		},
+	}
+};
+
+// How to retrieve configuration (use Console at Developer Tools in your browser)
+console.warn( config.fabrics["my-color-1"].rgb ); // or config['fabrics']["my-color-1"]["rgb"]
+console.warn( config.rules.lotus_xpto.config["upper-front"]["available-colors"] ); // or config["rules"]["lotus_xpto"]["config"]["upper-front"]["available-colors"]
+
+
+
+
+
+
+
+
+
 var lotus_petroleo = '{ "Colors" : [ '+
 	'{ "SECTOR_ID":"1", "RGB":"#FF00CC", "PANTONE":"XXXX","TISSUE":"XYZ 00" },'+
 	'{ "SECTOR_ID":"2", "RGB":"#00CCFF", "PANTONE":"XXYY","TISSUE":"XYZ 01" },'+
